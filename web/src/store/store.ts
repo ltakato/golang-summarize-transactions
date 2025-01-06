407bf022cc3c1ddae7fe84940338a4746f1b52a1
takato <ltakato@hotmail.com>
2025-01-06 16:27:08 -0300

    refactor: segment store defitinions step by step

--- web/src/store/store.ts
+++ web/src/store/store.ts
@@ -1,26 +1,31 @@
-import { create } from "zustand";
+import { create, StateCreator } from "zustand";
 import { devtools, subscribeWithSelector } from "zustand/middleware";
 import { Category } from "@/app/app-types";
 
-interface AppState {
+type AppStoreState = {
   currentDate: string | null;
-  setCurrentDate: (date: string | null) => void;
   currentCategory: Category | null;
+};
+type AppStoreActions = {
+  setCurrentDate: (date: string | null) => void;
   setCurrentCategory: (category: Category | null) => void;
-}
+};
+type AppStore = AppStoreState & AppStoreActions;
 
-export const useStore = create<AppState>()(
-  devtools(
-    subscribeWithSelector((set) => ({
-      currentDate: null,
-      setCurrentDate: (date: string | null) =>
-        set(() => ({ currentDate: date })),
-      currentCategory: null,
-      setCurrentCategory: (category: Category | null) =>
-        set(() => ({ currentCategory: category })),
-    })),
-  ),
-);
+const initialState: AppStoreState = {
+  currentDate: null,
+  currentCategory: null,
+};
+const storeInitializer: StateCreator<AppStore> = (set) => ({
+  ...initialState,
+  setCurrentDate: (date: string | null) => set(() => ({ currentDate: date })),
+  setCurrentCategory: (category: Category | null) =>
+    set(() => ({ currentCategory: category })),
+});
+const withSubscribeWithSelector = subscribeWithSelector(storeInitializer);
+const withDevtools = devtools(withSubscribeWithSelector);
+
+export const useStore = create<AppStore>()(withDevtools);
 
 useStore.subscribe(
   (state) => state.currentDate,
